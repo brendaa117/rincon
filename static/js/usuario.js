4f79756799ab098a51ba9cc9e6327eb2a089026c
alisontirado <alisongt06@gmail.com>
2025-05-21 23:32:08 -0600

    Create usuario.js

---
+++ static/js/usuario.js
@@ -0,0 +1,6 @@
+// Aquí puedes añadir interactividad si lo necesitas
+// Por ejemplo, abrir otra página al hacer clic en la imagen:
+
+document.querySelector('.more img')?.addEventListener('click', () => {
+    window.location.href = 'usuario.html'; // ejemplo
+});
